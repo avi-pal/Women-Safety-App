c3d99600f83602753a7423a0d69debf38a83f313
Avirup Pal <avirup150@gmail.com>
2023-08-17 22:02:07 +0530

    message

--- lib/child/bottom_screens/add_contacts.dart
+++ lib/child/bottom_screens/add_contacts.dart
@@ -81,7 +81,7 @@ class _AddContactsPageState extends State<AddContactsPage> {
                             IconButton(
                                 onPressed: () async {
                                   await FlutterPhoneDirectCaller.callNumber(
-                                      contactList![index].number);
+                                      contactList!g[index].number);
                                 },
                                 icon: Icon(
                                   Icons.call,
